143f7697f712ad9622370ff2597f966977cafb5f
Ankit Bahuguna <ankit.bahuguna@yapits.com>
2018-02-06 18:21:32 +0530

    Clean Up

--- script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-PID=$(pgrep gnome-session)
-export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-);/home/ankit/.nvm/versions/node/v8.9.1/bin/node /home/ankit/projects/xkcd/index.js
